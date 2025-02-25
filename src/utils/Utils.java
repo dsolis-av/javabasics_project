225323e6e6c845776856757d4e7c37bdbb43146b
dsolis-av <d.solis@globant.com>
2025-02-24 23:45:32 -0500

    Added code comment to explain the purpose of the static Random instance in the Utils class

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -3,5 +3,6 @@ package utils;
 import java.util.Random;
 
 public class Utils {
+    //here we define a shared Random static instance to avoid creating multiple objects everytime we wanted to generate random numbers
     public static Random rand = new Random();
 }
